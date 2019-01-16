be0482a79a577964950557c7e250b69b5ada2786
Douglas Mariano Silverio <douglas07@gmail.com>
2019-01-15 23:44:46 -0200

    desafio 4

--- dom/desafio-04/app.js
+++ dom/desafio-04/app.js
@@ -1,18 +1,38 @@
 new Vue({
   el: '#desafio',
   data: {
-    classe1:'destaque'
+    classe1: 'destaque',
+    aplicarLinha: true,
+    classeCSS: 'azul',
+    classeCSS2: 'linha',
+    cor: 'blue',
+    width: '1%',
+    altura: 50
 
   },
   methods: {
-    iniciarEfeito: function() {
-     setInterval(() => {
-       this.classe1 = this.classe1 == 'destaque'
-       ?'encolher' : 'destaque'
-     }, 1000);
+    iniciarEfeito: function () {
+      setInterval(() => {
+        this.classe1 = this.classe1 == 'destaque' ?
+          'encolher' : 'destaque'
+      }, 1000);
     },
-    startProgress(){
-
+    startProgress() {
+      let valor =0;
+      const temporizadpr =    setInterval(() => {
+         valor+=5;
+       this.width = `${valor}%`
+        if(valor==100) clearInterval(temporizadpr)
+      }, 100);
     }
-  }
+  },
+  computed: {
+    barra(){
+      return {
+        width: `${this.tamanho}px`,
+        height: `${this.altura}px`,
+        backgroundColor:this.cor
+      }
+    }
+  },
 });
