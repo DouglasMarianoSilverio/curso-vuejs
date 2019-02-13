88959c325b2d4f8b408b67c6a799cdcce89d69b2
Douglas Mariano Silverio <douglas07@gmail.com>
2019-02-12 23:33:24 -0200

    tratamento da cor da barra de energia. inicio da div de mensagem ao usuario. falta tratar final do jogo e log de mensagens

--- proj01_assasino_mostros/app.js
+++ proj01_assasino_mostros/app.js
@@ -3,7 +3,10 @@ new Vue({
     data :{
   
     iEnergiaJogador : 100,
-    iEnergiaMostro : 100,
+    iEnergiaMonstro : 100,
+    mensagem:null,
+
+    jogoIniciado : false,
 
     },
     watch: {
@@ -14,24 +17,46 @@ new Vue({
           return `${this.iEnergiaJogador}%`
         },
         energiaMostro(){
-            return `${this.iEnergiaMostro}%`
-        }
-    },
+            return `${this.iEnergiaMonstro}%`
+        },
+        barraJogador(){
+            return {
+                   'bg-success':this.iEnergiaJogador >20,
+                   'bg-danger':this.iEnergiaJogador <= 20
+                }
+            },
+            barraMonstro(){
+                return {
+                       'bg-success':this.iEnergiaMonstro >20,
+                       'bg-danger':this.iEnergiaMonstro <= 20
+                    }
+                }
+        },
     methods: {
         Ataque(){
             this.iEnergiaJogador -= Math.floor(Math.random() * 7);
-            this.iEnergiaMostro -=  Math.floor(Math.random() * 5);
+            this.iEnergiaMonstro -=  Math.floor(Math.random() * 5);
 
         },
         AtaqueEspecial(){
             this.iEnergiaJogador-= Math.floor(Math.random() * 7);
-            this.iEnergiaMostro -= Math.floor(Math.random() * 10);
+            this.iEnergiaMonstro -= Math.floor(Math.random() * 10);
 
         },
         Curar(){
             this.iEnergiaJogador +=   Math.floor(Math.random() * 7);
             this.iEnergiaJogador -= Math.floor(Math.random() * 7);
 
+        },
+        Desistir(){
+                this.mensagem="Você desistiu";
+                this.jogoIniciado=false;
+        },
+        IniciarJogo(){
+            this.jogoIniciado = true;
+            this.iEnergiaJogador=100;
+            this.iEnergiaMonstro=100;
+            this.mensagem=null;
         }
     },
 
