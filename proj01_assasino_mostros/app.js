39b7c60edfbd0ad5e79d3cb1d17412402a0710b8
Douglas Mariano Silverio <douglas.silverio@sistemapoliedro.com.br>
2019-05-09 10:50:29 -0300

    teste 1

--- proj01_assasino_mostros/app.js
+++ proj01_assasino_mostros/app.js
@@ -1,62 +1,62 @@
 new Vue({
-    el : "#app",
-    data :{
-  
-    iEnergiaJogador : 100,
-    iEnergiaMonstro : 100,
-    mensagem:null,
+    el: "#app",
+    data: {
 
-    jogoIniciado : false,
+        iEnergiaJogador: 100,
+        iEnergiaMonstro: 100,
+        mensagem: null,
+
+        jogoIniciado: false,
 
     },
     watch: {
-        
+
     },
     computed: {
-        energiaJogador(){
-          return `${this.iEnergiaJogador}%`
+        energiaJogador() {
+            return `${this.iEnergiaJogador}%`
         },
-        energiaMostro(){
+        energiaMostro() {
             return `${this.iEnergiaMonstro}%`
         },
-        barraJogador(){
+        barraJogador() {
             return {
-                   'bg-success':this.iEnergiaJogador >20,
-                   'bg-danger':this.iEnergiaJogador <= 20
-                }
-            },
-            barraMonstro(){
-                return {
-                       'bg-success':this.iEnergiaMonstro >20,
-                       'bg-danger':this.iEnergiaMonstro <= 20
-                    }
-                }
+                'bg-success': this.iEnergiaJogador > 20,
+                'bg-danger': this.iEnergiaJogador <= 20
+            }
         },
+        barraMonstro() {
+            return {
+                'bg-success': this.iEnergiaMonstro > 20,
+                'bg-danger': this.iEnergiaMonstro <= 20
+            }
+        }
+    },
     methods: {
-        Ataque(){
+        Ataque() {
             this.iEnergiaJogador -= Math.floor(Math.random() * 7);
-            this.iEnergiaMonstro -=  Math.floor(Math.random() * 5);
+            this.iEnergiaMonstro -= Math.floor(Math.random() * 5);
 
         },
-        AtaqueEspecial(){
-            this.iEnergiaJogador-= Math.floor(Math.random() * 7);
+        AtaqueEspecial() {
+            this.iEnergiaJogador -= Math.floor(Math.random() * 7);
             this.iEnergiaMonstro -= Math.floor(Math.random() * 10);
 
         },
-        Curar(){
-            this.iEnergiaJogador +=   Math.floor(Math.random() * 7);
+        Curar() {
+            this.iEnergiaJogador += Math.floor(Math.random() * 7);
             this.iEnergiaJogador -= Math.floor(Math.random() * 7);
 
         },
-        Desistir(){
-                this.mensagem="Você desistiu";
-                this.jogoIniciado=false;
+        Desistir() {
+            this.mensagem = "Você desistiu";
+            this.jogoIniciado = false;
         },
-        IniciarJogo(){
+        IniciarJogo() {
             this.jogoIniciado = true;
-            this.iEnergiaJogador=100;
-            this.iEnergiaMonstro=100;
-            this.mensagem=null;
+            this.iEnergiaJogador = 100;
+            this.iEnergiaMonstro = 100;
+            this.mensagem = null;
         }
     },
 
